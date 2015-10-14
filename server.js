de1bc0b928645903ff361e626d2eb3e0fac8fd21
hugoender <hugoender42@gmail.com>
2015-10-14 19:11:33 +0000

    Add clickHandler controller

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ var express = require('express'),
 var app = express();
 
 mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+
     if (err) {
         throw new Error('Database failed to connect!');
     } else {
         console.log('MongoDB successfully connected on port 27017.');
     }
-    
+
     app.use('/public', express.static(process.cwd() + '/public'));
     app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-    
+
     routes(app, db);
-    
-    app.listen(8080, function() {
+
+    app.listen(8080, function () {
         console.log('Listening on port 8080...');
     });
-});
 
+});
