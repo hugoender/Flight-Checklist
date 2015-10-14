825eb0fba16a8958b8df276e24633d37f3486a02
hugoender <hugoender42@gmail.com>
2015-10-14 20:04:55 +0000

    Add addClick and resetClicks functions insides clickHandler function

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -13,8 +13,44 @@ function clickHandler (db) {
                 throw err;
             }
             
-            res.json(result);
+            if (result) {
+                res.json(result);
+            } else {
+                clicks.insert({ 'clicks': 0 }, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    
+                    clicks.findOne({}, clickProjection, function (err, doc) {
+                        if (err) {
+                            throw err;
+                        }
+                        
+                        res.json(doc);
+                    });
+                });
+            }
         });
+    };
+    
+    this.addClick = function (req, res) {
+        clicks.findAndModify({}, { '_id': 1 }, { $inc: { 'clicks': 1 } },
+            function (err, result) {
+                if (err) { throw err; }
+                
+                res.json(result);
+            }
+        );
+    };
+    
+    this.resetClicks = function (req, res) {
+        clicks.update( {}, {'clicks': 0 },
+            function (err, result) {
+                if (err) { throw err; }
+                    
+                res.json(result);
+            }
+        );
     };
 }
 
