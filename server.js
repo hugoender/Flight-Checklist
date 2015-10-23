1c6a67e70dfc27bc76512b2b3e96e238ab1ca0a1
hugoender <hugoender42@gmail.com>
2015-10-23 18:53:44 -0400

    Setup initial app structure

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Include dependencies/modules
+var express = require('express'),
+    mongo = require('mongodb').MongoClient;
+
+// Assign express object (and it's methods) to app
+var app = express();
