c8a94affb559d5acbf0f8995c69634145a6f4b7b
hugoender <hugoender42@gmail.com>
2015-11-21 21:40:08 -0500

    Change port, dburl, and apiurl to environment variables

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var routes = require('./server/routes/index.js');
 var app = express();
 
 // URLs and ports ==============================================================
-var port = 3000;
+var port = process.env.PORT || 3000;
 var mongoport = 27017;
-var dburl = 'mongodb://localhost:' + mongoport + '/checklistapp';
+var dburl = process.env.MONGOLAB_URI || 'mongodb://localhost:' + mongoport + '/checklistapp';
 
 // Connect to MongoDB ==========================================================
 mongo.connect(dburl, function(err, db){
--- server/controllers/controller.client.js
+++ server/controllers/controller.client.js
@@ -14,7 +14,7 @@
 // });
 
 // Define API endpoint -------------------------------------------------------
-var apiURL = 'http://localhost:3000/api/logs';
+var apiURL = window.location.origin || 'http://localhost:3000/api/logs';
 
 // AJAX request (VanillaJS) --------------------------------------------------
 // function ajaxRequest(method, url, type, id, callback) {
